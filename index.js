805a5ad39e5072817b6ee12457a25e47f23ed192
shubham gaikwad <shubhamgaikwad2k4@gmail.com>
2024-11-16 10:47:18 +0530

    changes as per verccel

--- index.js
+++ index.js
@@ -1,55 +1,47 @@
-const express = require('express')
+const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors')
-const app = express()
-require('dotenv').config()
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const uploadImage = require("./src/utils/uploadImage");
 
-const port = process.env.PORT || 5000
+// Create Express app
+const app = express();
 
-//middlewares
-app.use(express.json({ limit: "25mb" }))
-// app.use(express.urlencoded({ limit: "25mb" }))
-app.use(cookieParser())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+// Middlewares
+app.use(express.json({ limit: "25mb" }));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
     origin: 'http://localhost:5173',
-    credentials: true
-}))
+    credentials: true,
+}));
 
-//all routes
-const authRoutes = require('./src/users/user.route')
-const productRoutes = require('./src/products/products.route')
+// All routes
+const authRoutes = require('./src/users/user.route');
+const productRoutes = require('./src/products/products.route');
+app.use('/api/auth', authRoutes);
+app.use('/api/products', productRoutes);
 
-app.use('/api/auth', authRoutes)
-app.use('/api/products', productRoutes)
+// Database connection
+mongoose.connect(process.env.DB_URL)
+    .then(() => console.log("MongoDB successfully connected"))
+    .catch((err) => console.error(err));
 
-main().then(() => console.log("Mongodb successfully connected")).catch(err => console.log(err));
+// Default route
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
-// mongo - pass : admin123
-// username : admin
-
-async function main() {
-    await mongoose.connect(process.env.DB_URL);
-
-    app.get('/', (req, res) => {
-        res.send('Hello World!')
-    })
-}
-
-
-// upload image routes
+// Upload image route
 app.post("/uploadImage", (req, res) => {
     uploadImage(req.body.image)
         .then((url) => res.send(url))
         .catch((err) => res.status(500).send(err));
 });
 
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+// Export app for Vercel
+module.exports = app;
