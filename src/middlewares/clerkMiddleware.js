5311da136da02f52ca16d01c20f47d9ed712476f
shubham gaikwad <shubhamgaikwad2k4@gmail.com>
2024-11-22 22:22:53 +0530

    clerk middleware removed

--- src/middlewares/clerkMiddleware.js
+++
@@ -1,6 +0,0 @@
-const { requireAuth } = require('@clerk/clerk-sdk-node');
-
-// Example: Protecting a route that requires authentication
-app.get('/protected', requireAuth(), (req, res) => {
-    res.send('This is a protected route!');
-});
