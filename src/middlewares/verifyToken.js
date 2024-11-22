13efcb4287be5fb206278e48f7d724b5025b6684
shubham gaikwad <shubhamgaikwad2k4@gmail.com>
2024-11-22 19:34:29 +0530

    update token

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -4,7 +4,7 @@ const JWT_SECRET = process.env.JWT_SECRET_KEY;
 
 const verifyToken = (req, res, next) => {
     try {
-        const token = req.cookies?.token || req.headers.authorization?.split(" ")[1];
+        const token = req.headers.authorization?.split(" ")[1] || req.cookies?.token;
         console.log("Token from request:", token);
 
 
